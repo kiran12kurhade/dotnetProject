6f0632e5af7af536611b6d3b4f92757346bb5b01
kiran12kurhade^cgit config --global user.name kiran12kurhade@gmail.com <101124371+kiran12kurhade@users.noreply.github.com>
2024-01-12 23:42:45 +0530

    Add project files.

---
+++ Day10/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Day10.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Day10.Data
+{
+    public class MyDbContext:DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
